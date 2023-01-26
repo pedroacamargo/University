c44963287ca531aa549e19ebef45f52778c24581
pedroacamargo <pedroaugennes@gmail.com>
2023-01-26 16:55:43 +0000

    Update Teste2022-2023.hs

--- 1 ano/programacao-funcional/Testes/Teste2022-2023.hs
+++ 1 ano/programacao-funcional/Testes/Teste2022-2023.hs
@@ -28,13 +28,13 @@ type Mat = [[Int]]
 
 -- Transposta em formato textual: "2,12,3 \n 3,3,-4 \n 6,12,5 \n 4,4,7"
 
-transposta :: String -> String
-transposta m = toString (transform m)
+--transposta :: String -> String
+--transposta m = toString (transform m)
 
 -- temos de pegar a string e transformar em Mat, enquanto também pegamos a cabeça de cada lista da Mat e fazemos uma lista de cabeças (para ser a transposta)
-transform :: String -> Mat
-transform [] = []
-transform (h:t) = map (head) (stringToMat (h:t)) : transform (map (tail) (stringToMat (h:t)))
+--transform :: String -> Mat
+--transform [] = []
+--transform (h:t) = map (head) (stringToMat (h:t)) : transform (map (tail) (stringToMat (h:t)))
 
 -- Depois transformamos a data Mat em string de volta, de forma que fique igual ao formato textual. O show irá transformar os ints em string 
 toString :: Mat -> String
@@ -45,7 +45,7 @@ toString m = show (head m) ++ "\n" ++ toString (tail m)
 
 ----------------------------------------------------------------------------
 --3) Feita por Pedro Augusto Camargo :)
-data Lista a = Esq a (Lista a) | Dir (Lista a) a | Nula deriving Show
+data Lista a = Esq a (Lista a) | Dir (Lista a) a | Nula 
 
 lista' :: Num a => Lista a
 lista' = Dir (Esq 1 (Dir (Dir Nula 2) 5)) 2
@@ -83,3 +83,41 @@ instance Show a => Show (Lista a) where
 
 showArv :: Show a => Lista a -> String
 showArv a = "[" ++ show a ++ "]"
+
+-----------------------------------------------------------------------------------------
+--4)
+{-
+TRAVESSIAS
+esq - pai - dir ===> infixa
+esq - dir - pai ===> posfixa
+pai - esq - dir ===> perfixa
+-}
+
+data BTree a = Empty | Node a (BTree a) (BTree a)
+
+--a) Não tenho certeza se está certa
+numera :: BTree a -> BTree (a,Int)
+numera (Empty) = Empty
+numera (Node r ae ad) = (Node (r, fst (numeraAux 1 (Node r ae ad))) (numera ae) (numera ad))
+
+numeraAux :: Int -> BTree a -> (Int,BTree (a,Int))
+numeraAux x (Empty) = (x,Empty)
+numeraAux x (Node r Empty ad) = (1, (Node (r,x) Empty (continuarArv (x+1) ad)))
+numeraAux x (Node r ae Empty) = (1, (Node (r,x) (continuarArv (x+1) ae) Empty))
+numeraAux x (Node r ae ad) = (1 - contarAux ae + contarAux ad, (Node (r,x) (continuarArv (x-1) ae) (continuarArv (x+1) ad)))
+
+contarAux :: BTree a -> Int
+contarAux Empty = 0
+contarAux (Node r ae ad) = 1 + contarAux ae + contarAux ad
+
+continuarArv :: Int -> BTree a -> BTree (a,Int)
+continuarArv x (Empty) = Empty
+continuarArv x (Node r ae ad) = (Node (r,x) (continuarArv (x-1) ae) (continuarArv (x+1) ad))
+
+--b)
+inorder :: BTree a -> [a]
+inorder Empty = []
+inorder (Node r e d) = (inorder e) ++ (r:inorder d)
+
+unInorder :: [a] -> [BTree a]
+unInorder (h:t) | 
