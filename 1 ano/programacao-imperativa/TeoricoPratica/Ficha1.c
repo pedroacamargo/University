e6a1d0e77bda92be9020c0c3773df42fac662556
Pedro Augusto <98715404+pedroacamargo@users.noreply.github.com>
2023-02-16 01:27:16 +0000

    Ficha 1

--- 1 ano/programacao-imperativa/TeoricoPratica/Ficha1.c
+++ 1 ano/programacao-imperativa/TeoricoPratica/Ficha1.c
@@ -33,8 +33,96 @@ void quadradoBrabo() {
 
 }
 
-int main() {
-    quadradoBrabo();
+// 3.2)
+void xadrez() {
+    int x;
+    int contador = 0;
+    int altura = 0;
+    printf("Digite um numero para as colunas e para as linhas de um tabuleiro de xadrez: ");
+    scanf("%d", &x);
+    while (contador <= x) {
+        if (contador % 2 == 0) {
+            if (contador == x) {
+                printf("\n");
+                contador = 0;
+                altura += 1;
+            } else {
+                printf("#");
+                contador += 1;
+            }
+        } else {
+            if (contador == x) {
+                printf("\n");
+                contador = 0;
+                altura += 1;
+            } else {
+                printf("_");
+                contador += 1;
+            }
+        }
 
+        if (altura == x){
+            break;
+        }
+    }
+}
+
+void trianguloBrabo () {
+    int x,y,z;
+    int contador, contador2;
+    int colunas;
+    char primeiro = 's';
+    printf("Escolha uma altura para o triangulo: ");
+    scanf("%d", &x);
+    contador = 0;
+    contador2 = x;
+    z = x;
+    y = 1;
+    printf("#\n");
+    while (contador2 != 0) {
+        while (contador <= y + 1) {
+            printf("#");
+            contador += 1;
+            if (contador == y + 1) {
+                printf("\n");
+                y += 1;
+                contador = 0;
+            }
+            if (contador2 == y) {
+                contador = y + 2;
+                break;
+            }
+
+        }
+        int contador3 = contador2;
+        while (contador2 <= y) {
+            if (primeiro == 's') {
+                contador2 -= 1;
+                primeiro = 'a';
+            }
+            printf("#");
+            contador2 -= 1;
+            if (contador2 == 0) {
+                contador3 -= 1;
+                printf("\n");
+                contador2 = y-1;
+                y -= 1;
+                if (contador3 == 0) {
+                    break;
+                }
+            } 
+            
+        }
+        
+        break;
+    }
+}
+
+
+int main() {
+
+    //quadradoBrabo(); -> 3.1
+    // xadrez(); -> 3.2 
+    trianguloBrabo();
     return 0;  
 }
--- 1 ano/programacao-imperativa/teorica/HelloWorld.c
+++ 1 ano/programacao-imperativa/teorica/HelloWorld.c
@@ -1,9 +1,5 @@
 #include <stdio.h>
 
-void tres_um(){
-    dusadasda;
-}
-
 int main() {
     printf("Hello World\n");
 
