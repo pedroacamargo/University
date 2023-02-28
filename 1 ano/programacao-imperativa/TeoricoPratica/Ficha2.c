f936a7cce5cfdc6e59bde275c1732f6b23f05555
Pedro Augusto <98715404+pedroacamargo@users.noreply.github.com>
2023-02-28 14:52:12 +0000

    Ficha 2

--- 1 ano/programacao-imperativa/TeoricoPratica/Ficha2.c
+++ 1 ano/programacao-imperativa/TeoricoPratica/Ficha2.c
@@ -30,11 +30,52 @@ float multInt2(int n, float m) {
     return m + imparN;
 }
 
+// 3)
+int verMaiorDivisor(int a) {
+    
+
+    for (int i = a - 1; i != 0; i--) {
+        if (i == 1) {
+            return 0;
+        }
+        if (a % i == 0 && i != 1) {
+            return i;
+        }
+    }
+}
+
+int mdc1(int a, int b) {
+    int maiorDivisorA = verMaiorDivisor(a); // 7
+    int maiorDivisorB = verMaiorDivisor(b); // 0
+    int temp = a * b;
+    // mdc 8,4 = 2 x 2 x 2 = 8
+    // mdc 16, 14 = 2 x 2 x 2 x 2 x 7
+    // mdc 15 = 5
+    if (a % b == 0) {
+        if (a < b) {
+            printf("Passei aqui");
+            return b;
+        } else {
+            printf("Passei aqui");
+            return a;
+        }
+    } else if (maiorDivisorA > maiorDivisorB) {
+        maiorDivisorA = verMaiorDivisor(maiorDivisorA);
+    } else if (maiorDivisorA < maiorDivisorB) {
+        maiorDivisorB = verMaiorDivisor(maiorDivisorB);
+    } else if (maiorDivisorA == 0 || maiorDivisorB == 0) {
+        printf("Passei aqui");
+        return temp;
+    }
+}
+
 
 
 int main(){
     float res1 = multInt1(10,2.0);
     float res2 = multInt2(81,423);
-    printf("%.0f",res2);
+    int res3 = mdc1(15,5);
+    printf("%d", res3);
+    // printf("%.0f",res3);
     return 0;
 }
