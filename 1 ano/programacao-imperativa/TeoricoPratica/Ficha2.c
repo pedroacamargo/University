74c0b5695cfcf7cdfdb456e88ac5d2a501c6dc28
Pedro Augusto <98715404+pedroacamargo@users.noreply.github.com>
2023-03-01 10:02:03 +0000

    ficha 2

--- 1 ano/programacao-imperativa/TeoricoPratica/Ficha2.c
+++ 1 ano/programacao-imperativa/TeoricoPratica/Ficha2.c
@@ -32,11 +32,9 @@ float multInt2(int n, float m) {
 
 // 3)
 int verMaiorDivisor(int a) {
-    
-
     for (int i = a - 1; i != 0; i--) {
         if (i == 1) {
-            return 0;
+            return 1;
         }
         if (a % i == 0 && i != 1) {
             return i;
@@ -45,18 +43,16 @@ int verMaiorDivisor(int a) {
 }
 
 int mdc1(int a, int b) {
+    int temp = a * b;
     int maiorDivisorA = verMaiorDivisor(a); // 7
     int maiorDivisorB = verMaiorDivisor(b); // 0
-    int temp = a * b;
     // mdc 8,4 = 2 x 2 x 2 = 8
     // mdc 16, 14 = 2 x 2 x 2 x 2 x 7
     // mdc 15 = 5
     if (a % b == 0) {
         if (a < b) {
-            printf("Passei aqui");
             return b;
         } else {
-            printf("Passei aqui");
             return a;
         }
     } else if (maiorDivisorA > maiorDivisorB) {
@@ -64,18 +60,36 @@ int mdc1(int a, int b) {
     } else if (maiorDivisorA < maiorDivisorB) {
         maiorDivisorB = verMaiorDivisor(maiorDivisorB);
     } else if (maiorDivisorA == 0 || maiorDivisorB == 0) {
-        printf("Passei aqui");
         return temp;
     }
 }
 
-
+// substituir o maior dos argumentos pela diferença entre eles até que seja 0 || 
+int mdc2(int a, int b) {
+    int c; 
+    if (a >= b) {
+        if (a - b == 0) {
+            c = a;
+            return c;
+        }
+        a -= b;
+    } else if (a <= b) {
+        if (b - a == 0) {
+            c = b;
+            return c;
+        }
+        b -= a;
+    } else if (a == 0 || b == 0) {
+        return c;
+    }
+}
 
 int main(){
     float res1 = multInt1(10,2.0);
     float res2 = multInt2(81,423);
-    int res3 = mdc1(15,5);
-    printf("%d", res3);
+    int res3 = mdc1(21,2);
+    int res4 = mdc2(126,45);
+    printf("%d", res4);
     // printf("%.0f",res3);
     return 0;
 }
