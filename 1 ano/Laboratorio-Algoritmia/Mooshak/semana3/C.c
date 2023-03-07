81e127a72ab26682c6192f4da4414f0f97753ca2
pedroacamargo <pedroaugennes@gmail.com>
2023-03-07 15:52:12 +0000

    C.c

--- 1 ano/Laboratorio-Algoritmia/Mooshak/semana3/C.c
+++ 1 ano/Laboratorio-Algoritmia/Mooshak/semana3/C.c
@@ -2,38 +2,37 @@
 #include <string.h> 
 
 int main() {
-	int N = 0;
-	int K - 0;
-	int linha[200];
-
+	int n;
+	int K = 0;
+	int atual = 0;
 	int index = 0;
-
 
 	int maior = 0;
 
-	if (scanf("%d", &N) != 1) return 1;
+	if (scanf("%d", &n) != 1) return 1;
 
-	for (int i = 0; i < N + 1; i++) { // linhas
+	int arrayMaiores[n];
 
+	for (int i = 0; i < n; i++) { // linhas
 		if (scanf("%d", &K) != 1) return 1;
 
-		for (int j = 0; i < K; i++) {
+		for (int j = 0; j < K; j++) {
+
 			if (scanf("%d", &atual) != 1) return 1;
 
 			if (atual > maior) {
-				maior = j;
-			} 
+				index++;
+				maior = atual;
+			}
 		}
-	
-		printf("%d",linha);		
-
-
-
-
-
-
-
+		arrayMaiores[i] = index;
+		maior = 0;
+		index = 0;
 	}	
+
+	for (int x = 0; x < n; x++) {
+		printf("%d\n", arrayMaiores[x]);
+	}
 
 	return 0;
 }
