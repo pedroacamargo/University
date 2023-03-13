d682ec8cd8f3069c26d8b24d3906193e1e60629e
pedroacamargo <pedroaugennes@gmail.com>
2023-03-13 11:20:04 +0000

    ola moniz

---
+++ 1 ano/Laboratorio-Algoritmia/Mooshak/semana4/A.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+int main() {
+   
+  int N;
+  char phrase[1001];
+
+  if (scanf("%d", &N) != 1) return 1;
+
+  int n = 0;
+  // linhas
+  for (int i = 0; i < N; i++) {
+    while (scanf("%s", phrase) != 1) {
+      
+    }
+
+
+
+  }
+
+  printf("%d", n);
+  return 0;
+}
