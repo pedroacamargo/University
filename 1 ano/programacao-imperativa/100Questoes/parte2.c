721263b9b46b95ff73a9c088b23be9347ce992f3
pedroacamargo <pedroaugennes@gmail.com>
2023-05-13 17:38:24 +0100

    PI 50Q p2

--- 1 ano/programacao-imperativa/100Questoes/parte2.c
+++ 1 ano/programacao-imperativa/100Questoes/parte2.c
@@ -169,6 +169,276 @@ int removeAll(LInt *l, int x) {
 }
 
 
+/* Questão 11 */
+int removeDups(LInt *l){
+	int count = 0;
+
+	for(; *l ; l = (&(*l)->prox) ){
+		LInt anterior = (*l);
+		LInt seguinte = (*l)->prox;
+		for(; seguinte ; seguinte = anterior->prox){
+			if( seguinte->valor == (*l)->valor ){
+				count += 1;
+				anterior->prox = seguinte->prox;
+				free(seguinte);
+			} else
+				anterior = seguinte;
+		}
+	}
+			
+	return count;
+}
+
+
+/* Questão 12 */
+int removeMaiorL(LInt *l) {
+    int max = (*l)->valor;
+    LInt prev = NULL;
+    LInt maxPrev = NULL;
+    LInt maxL = (*l);
+    LInt list = (*l);
+    for(;list; list = list->prox) {
+        if(list->valor > max) {
+            maxPrev = prev;
+            maxL = list;
+            max = list->valor;
+        }
+        prev = list;
+    }
+    if(!maxPrev) (*l) = (*l)->prox;
+    else maxPrev->prox = maxL->prox;
+    return max;
+}
+
+
+/* Questão 13 */
+void init (LInt * l) {
+  LInt prev = NULL;
+  while ((*l)->prox != NULL) {
+    prev = *l;
+    l = &( (*l)->prox);
+  }
+  if (!prev) {
+    free(*l);
+    *l = NULL;
+  } else {
+    prev->prox = NULL;
+    free(*l);
+  }
+}
+
+
+/* Questão 14 */
+void appendL (LInt * l, int x) {
+    LInt new = malloc(sizeof(struct lligada));
+    new->valor = x;
+    new->prox = NULL;
+    if(!(*l)) *l = new;
+    else {
+      while((*l)->prox) l = &( (*l)->prox); // While the next element of the list is != NULL, go to the next element 
+        (*l)->prox = new;
+    }
+}
+
+
+/* Questão 15 */
+void concatL (LInt *a, LInt b) {
+  while ((*a)->prox) {
+    a = &( (*a)->prox);
+  }
+
+  (*a)->prox = b;
+}
+
+
+/* Questão 16 */
+LInt cloneL (LInt l){
+	LInt new_list;
+	LInt *sitio;
+
+	sitio = &new_list;
+
+	while( l ){
+		*sitio = malloc(sizeof(struct lligada));
+		(*sitio)->valor = l->valor;
+		l = l->prox;
+		sitio = &( (*sitio)->prox );
+	}
+
+	*sitio = NULL;
+
+	return new_list;
+}
+
+
+/* Questão 17 */
+LInt cloneRev (LInt l) {
+    LInt list = NULL;
+    while(l) {
+        LInt new = malloc(sizeof(struct lligada));
+        new->valor = l->valor;
+        new->prox = list;
+        list = new;
+        l = l->prox;
+    }
+    return list;
+} 
+
+
+/* Questão 18 */
+int maximo (LInt l) {
+  int maximo = l->valor;
+  while (l) {
+    if (l->valor > maximo) maximo = l->valor;
+    l = l->prox;
+  }
+
+  return maximo;
+}
+
+
+/* Questão 19 */
+int take (int n, LInt *l) {
+  int tamanho = 0;
+  while (n != 0 && *l) {
+    l = &( (*l)->prox);
+    n--;
+    tamanho += 1;
+  }
+  if (!(l)) return tamanho;
+
+  while (*l) {
+    LInt temp = (*l)->prox;
+    free(*l);
+    *l = temp;
+  }
+  return tamanho;
+}
+
+
+/* Questão 20 */
+int drop(int n, LInt *l) {
+  if (!(*l)) return 0;
+  int rem = 0;
+  while (n > 0 && (*l)) {
+    LInt temp = (*l);
+    *l = (*l)->prox;
+    free(temp);
+    n--;
+    rem++;
+  }
+  return rem;
+}
+
+
+/* Questão 21 */
+LInt Nforward (LInt l, int N){
+  LInt laux = l;
+  int i = 0;
+  while (i != N) {
+    laux = laux->prox;
+    i++;
+  }
+  return laux;
+}
+
+
+/* Questão 22 */
+int listToArray (LInt l, int v[], int N) {
+  int i = 0;
+  while (l && i < N) {
+    v[i] = l->valor;
+    l = l->prox;
+    i++;
+  }
+  return i;
+}
+
+
+/* Questão 23 */
+LInt arrayToList (int v[], int N) {
+  LInt head;
+  LInt *r = &head;
+  int i = 0;
+  while (i < N) {
+    *r = malloc(sizeof(LInt));
+    (*r)->valor = v[i];
+    r = &(*r)->prox;
+  }
+  *r = NULL;
+  return head;
+}
+
+
+/* Questão 24 */
+LInt somasAcL (LInt l) {
+  LInt head;
+  LInt *r = &head;
+  int acc = 0;
+  while (l) {
+    acc += l->valor;
+    l = l->prox;
+    *r = malloc(sizeof(LInt));
+    (*r)->valor = acc;
+    r = &(*r)->prox;
+  }
+  *r = NULL;
+  return head;
+}
+
+
+/* Questão 25 */
+void remreps (LInt l) {
+  while(l->prox) {
+    if(l->prox->valor == l->valor) {
+        LInt temp = l->prox;
+        l->prox = temp->prox;
+        free(temp);
+    }
+    else l = l->prox;
+  }
+}
+
+
+/* Questão 26 */
+LInt rotateL(LInt l){
+	
+	if(!l || !(l->prox))
+		return l;
+	
+	LInt aux = l;
+	LInt head = l->prox;
+
+	while(aux->prox)
+		aux = aux->prox;
+
+	aux->prox = l;
+	l->prox = NULL;
+
+	return head;
+}
+
+
+/* Questão 27 */
+LInt parte (LInt l){
+  LInt head, *p = &head, *i = &l;
+  int j;
+  for (j = 1; *i; j++) {
+    if (!(j%2)) {
+      *p = *i;
+      *i = (*i)->prox;
+      p = &(*p)->prox;
+      
+    }
+    else i = &(*i)->prox;
+  }
+  *p = NULL;
+  return head;
+}
+
+
+
+
 
 /* Auxiliary functions */
 LInt getLInt(int len, int lnum) {
