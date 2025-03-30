2f2daa944ca964fc81edf178b21fb45cc29f956e
pedroacamargo <pedroaugennes@gmail.com>
2025-03-30 02:24:51 +0100

    Aula 6 CG

---
+++ 3 ano/CG/aula06/build/CMakeFiles/class6.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/class6.dir/link.d"
+  "CMakeFiles/class6.dir/main.cpp.o"
+  "CMakeFiles/class6.dir/main.cpp.o.d"
+  "class6"
+  "class6.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/class6.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ 3 ano/CG/aula06/build/CMakeFiles/class6.dir/link.d
@@ -0,0 +1,316 @@
+class6: \
+  /usr/lib/gcc/x86_64-linux-gnu/13/../../../x86_64-linux-gnu/Scrt1.o \
+  /usr/lib/gcc/x86_64-linux-gnu/13/../../../x86_64-linux-gnu/crti.o \
+  /usr/lib/gcc/x86_64-linux-gnu/13/crtbeginS.o \
+  CMakeFiles/class6.dir/main.cpp.o \
+  /usr/lib/x86_64-linux-gnu/libGL.so \
+  /usr/lib/x86_64-linux-gnu/libGLU.so \
+  /usr/lib/x86_64-linux-gnu/libglut.so \
+  /usr/lib/x86_64-linux-gnu/libXmu.so \
+  /usr/lib/x86_64-linux-gnu/libXi.so \
+  /usr/lib/x86_64-linux-gnu/libGLEW.so \
+  /usr/lib/x86_64-linux-gnu/libIL.so \
+  /usr/lib/gcc/x86_64-linux-gnu/13/libstdc++.so \
+  /usr/lib/gcc/x86_64-linux-gnu/13/../../../x86_64-linux-gnu/libm.so \
+  /usr/lib/gcc/x86_64-linux-gnu/13/../../../x86_64-linux-gnu/libm.so \
+  /usr/lib/gcc/x86_64-linux-gnu/13/../../../x86_64-linux-gnu/libm.so \
+  /lib/x86_64-linux-gnu/libm.so.6 \
+  /lib/x86_64-linux-gnu/libmvec.so.1 \
+  /usr/lib/gcc/x86_64-linux-gnu/13/libgcc_s.so \
+  /usr/lib/gcc/x86_64-linux-gnu/13/libgcc_s.so \
+  /usr/lib/gcc/x86_64-linux-gnu/13/libgcc_s.so \
+  /usr/lib/gcc/x86_64-linux-gnu/13/../../../x86_64-linux-gnu/libgcc_s.so.1 \
+  /usr/lib/gcc/x86_64-linux-gnu/13/libgcc.a \
+  /usr/lib/gcc/x86_64-linux-gnu/13/libgcc.a \
+  /usr/lib/gcc/x86_64-linux-gnu/13/../../../x86_64-linux-gnu/libc.so \
+  /usr/lib/gcc/x86_64-linux-gnu/13/../../../x86_64-linux-gnu/libc.so \
+  /usr/lib/gcc/x86_64-linux-gnu/13/../../../x86_64-linux-gnu/libc.so \
+  /lib/x86_64-linux-gnu/libc.so.6 \
+  /usr/lib/x86_64-linux-gnu/libc_nonshared.a \
+  /lib64/ld-linux-x86-64.so.2 \
+  /usr/lib/gcc/x86_64-linux-gnu/13/libgcc_s.so \
+  /usr/lib/gcc/x86_64-linux-gnu/13/libgcc_s.so \
+  /usr/lib/gcc/x86_64-linux-gnu/13/libgcc_s.so \
+  /usr/lib/gcc/x86_64-linux-gnu/13/../../../x86_64-linux-gnu/libgcc_s.so.1 \
+  /usr/lib/gcc/x86_64-linux-gnu/13/libgcc.a \
+  /usr/lib/gcc/x86_64-linux-gnu/13/libgcc.a \
+  /usr/lib/gcc/x86_64-linux-gnu/13/crtendS.o \
+  /usr/lib/gcc/x86_64-linux-gnu/13/../../../x86_64-linux-gnu/crtn.o \
+  /lib/i386-linux-gnu/libGLdispatch.so.0 \
+  /usr/lib/i386-linux-gnu/libGLdispatch.so.0 \
+  /lib/x86_64-linux-gnu/libGLdispatch.so.0 \
+  /lib/i386-linux-gnu/libGLX.so.0 \
+  /usr/lib/i386-linux-gnu/libGLX.so.0 \
+  /lib/x86_64-linux-gnu/libGLX.so.0 \
+  /lib/x86_64-linux-gnu/libOpenGL.so.0 \
+  /lib/i386-linux-gnu/libX11.so.6 \
+  /usr/lib/i386-linux-gnu/libX11.so.6 \
+  /lib/x86_64-linux-gnu/libX11.so.6 \
+  /lib/i386-linux-gnu/libXxf86vm.so.1 \
+  /usr/lib/i386-linux-gnu/libXxf86vm.so.1 \
+  /lib/x86_64-linux-gnu/libXxf86vm.so.1 \
+  /usr/lib/x86_64-linux-gnu/libXi.so \
+  /lib/i386-linux-gnu/libtiff.so.6 \
+  /usr/lib/i386-linux-gnu/libtiff.so.6 \
+  /lib/x86_64-linux-gnu/libtiff.so.6 \
+  /lib/i386-linux-gnu/libpng16.so.16 \
+  /usr/lib/i386-linux-gnu/libpng16.so.16 \
+  /lib/x86_64-linux-gnu/libpng16.so.16 \
+  /lib/x86_64-linux-gnu/libmng.so.2 \
+  /lib/i386-linux-gnu/liblcms2.so.2 \
+  /usr/lib/i386-linux-gnu/liblcms2.so.2 \
+  /lib/x86_64-linux-gnu/liblcms2.so.2 \
+  /lib/i386-linux-gnu/libjpeg.so.8 \
+  /usr/lib/i386-linux-gnu/libjpeg.so.8 \
+  /lib/x86_64-linux-gnu/libjpeg.so.8 \
+  /lib64/ld-linux-x86-64.so.2 \
+  /lib/i386-linux-gnu/libxcb.so.1 \
+  /usr/lib/i386-linux-gnu/libxcb.so.1 \
+  /lib/x86_64-linux-gnu/libxcb.so.1 \
+  /lib/i386-linux-gnu/libXext.so.6 \
+  /usr/lib/i386-linux-gnu/libXext.so.6 \
+  /lib/x86_64-linux-gnu/libXext.so.6 \
+  /lib/i386-linux-gnu/libwebp.so.7 \
+  /usr/lib/i386-linux-gnu/libwebp.so.7 \
+  /lib/x86_64-linux-gnu/libwebp.so.7 \
+  /lib/i386-linux-gnu/libzstd.so.1 \
+  /usr/lib/i386-linux-gnu/libzstd.so.1 \
+  /lib/x86_64-linux-gnu/libzstd.so.1 \
+  /lib/i386-linux-gnu/liblzma.so.5 \
+  /usr/lib/i386-linux-gnu/liblzma.so.5 \
+  /lib/x86_64-linux-gnu/liblzma.so.5 \
+  /lib/x86_64-linux-gnu/libLerc.so.4 \
+  /lib/i386-linux-gnu/libjbig.so.0 \
+  /usr/lib/i386-linux-gnu/libjbig.so.0 \
+  /lib/x86_64-linux-gnu/libjbig.so.0 \
+  /lib/i386-linux-gnu/libdeflate.so.0 \
+  /usr/lib/i386-linux-gnu/libdeflate.so.0 \
+  /lib/x86_64-linux-gnu/libdeflate.so.0 \
+  /lib/i386-linux-gnu/libz.so.1 \
+  /usr/lib/i386-linux-gnu/libz.so.1 \
+  /lib/x86_64-linux-gnu/libz.so.1 \
+  /lib/i386-linux-gnu/libXau.so.6 \
+  /usr/lib/i386-linux-gnu/libXau.so.6 \
+  /lib/x86_64-linux-gnu/libXau.so.6 \
+  /lib/i386-linux-gnu/libXdmcp.so.6 \
+  /usr/lib/i386-linux-gnu/libXdmcp.so.6 \
+  /lib/x86_64-linux-gnu/libXdmcp.so.6 \
+  /lib/i386-linux-gnu/libsharpyuv.so.0 \
+  /usr/lib/i386-linux-gnu/libsharpyuv.so.0 \
+  /lib/x86_64-linux-gnu/libsharpyuv.so.0 \
+  /lib/i386-linux-gnu/libbsd.so.0 \
+  /usr/lib/i386-linux-gnu/libbsd.so.0 \
+  /lib/x86_64-linux-gnu/libbsd.so.0 \
+  /lib/i386-linux-gnu/libmd.so.0 \
+  /usr/lib/i386-linux-gnu/libmd.so.0 \
+  /lib/x86_64-linux-gnu/libmd.so.0
+
+/usr/lib/gcc/x86_64-linux-gnu/13/../../../x86_64-linux-gnu/Scrt1.o:
+
+/usr/lib/gcc/x86_64-linux-gnu/13/../../../x86_64-linux-gnu/crti.o:
+
+/usr/lib/gcc/x86_64-linux-gnu/13/crtbeginS.o:
+
+CMakeFiles/class6.dir/main.cpp.o:
+
+/usr/lib/x86_64-linux-gnu/libGL.so:
+
+/usr/lib/x86_64-linux-gnu/libGLU.so:
+
+/usr/lib/x86_64-linux-gnu/libglut.so:
+
+/usr/lib/x86_64-linux-gnu/libXmu.so:
+
+/usr/lib/x86_64-linux-gnu/libXi.so:
+
+/usr/lib/x86_64-linux-gnu/libGLEW.so:
+
+/usr/lib/x86_64-linux-gnu/libIL.so:
+
+/usr/lib/gcc/x86_64-linux-gnu/13/libstdc++.so:
+
+/usr/lib/gcc/x86_64-linux-gnu/13/../../../x86_64-linux-gnu/libm.so:
+
+/usr/lib/gcc/x86_64-linux-gnu/13/../../../x86_64-linux-gnu/libm.so:
+
+/usr/lib/gcc/x86_64-linux-gnu/13/../../../x86_64-linux-gnu/libm.so:
+
+/lib/x86_64-linux-gnu/libm.so.6:
+
+/lib/x86_64-linux-gnu/libmvec.so.1:
+
+/usr/lib/gcc/x86_64-linux-gnu/13/libgcc_s.so:
+
+/usr/lib/gcc/x86_64-linux-gnu/13/libgcc_s.so:
+
+/usr/lib/gcc/x86_64-linux-gnu/13/libgcc_s.so:
+
+/usr/lib/gcc/x86_64-linux-gnu/13/../../../x86_64-linux-gnu/libgcc_s.so.1:
+
+/usr/lib/gcc/x86_64-linux-gnu/13/libgcc.a:
+
+/usr/lib/gcc/x86_64-linux-gnu/13/libgcc.a:
+
+/usr/lib/gcc/x86_64-linux-gnu/13/../../../x86_64-linux-gnu/libc.so:
+
+/usr/lib/gcc/x86_64-linux-gnu/13/../../../x86_64-linux-gnu/libc.so:
+
+/usr/lib/gcc/x86_64-linux-gnu/13/../../../x86_64-linux-gnu/libc.so:
+
+/lib/x86_64-linux-gnu/libc.so.6:
+
+/usr/lib/x86_64-linux-gnu/libc_nonshared.a:
+
+/lib64/ld-linux-x86-64.so.2:
+
+/usr/lib/gcc/x86_64-linux-gnu/13/libgcc_s.so:
+
+/usr/lib/gcc/x86_64-linux-gnu/13/libgcc_s.so:
+
+/usr/lib/gcc/x86_64-linux-gnu/13/libgcc_s.so:
+
+/usr/lib/gcc/x86_64-linux-gnu/13/../../../x86_64-linux-gnu/libgcc_s.so.1:
+
+/usr/lib/gcc/x86_64-linux-gnu/13/libgcc.a:
+
+/usr/lib/gcc/x86_64-linux-gnu/13/libgcc.a:
+
+/usr/lib/gcc/x86_64-linux-gnu/13/crtendS.o:
+
+/usr/lib/gcc/x86_64-linux-gnu/13/../../../x86_64-linux-gnu/crtn.o:
+
+/lib/i386-linux-gnu/libGLdispatch.so.0:
+
+/usr/lib/i386-linux-gnu/libGLdispatch.so.0:
+
+/lib/x86_64-linux-gnu/libGLdispatch.so.0:
+
+/lib/i386-linux-gnu/libGLX.so.0:
+
+/usr/lib/i386-linux-gnu/libGLX.so.0:
+
+/lib/x86_64-linux-gnu/libGLX.so.0:
+
+/lib/x86_64-linux-gnu/libOpenGL.so.0:
+
+/lib/i386-linux-gnu/libX11.so.6:
+
+/usr/lib/i386-linux-gnu/libX11.so.6:
+
+/lib/x86_64-linux-gnu/libX11.so.6:
+
+/lib/i386-linux-gnu/libXxf86vm.so.1:
+
+/usr/lib/i386-linux-gnu/libXxf86vm.so.1:
+
+/lib/x86_64-linux-gnu/libXxf86vm.so.1:
+
+/usr/lib/x86_64-linux-gnu/libXi.so:
+
+/lib/i386-linux-gnu/libtiff.so.6:
+
+/usr/lib/i386-linux-gnu/libtiff.so.6:
+
+/lib/x86_64-linux-gnu/libtiff.so.6:
+
+/lib/i386-linux-gnu/libpng16.so.16:
+
+/usr/lib/i386-linux-gnu/libpng16.so.16:
+
+/lib/x86_64-linux-gnu/libpng16.so.16:
+
+/lib/x86_64-linux-gnu/libmng.so.2:
+
+/lib/i386-linux-gnu/liblcms2.so.2:
+
+/usr/lib/i386-linux-gnu/liblcms2.so.2:
+
+/lib/x86_64-linux-gnu/liblcms2.so.2:
+
+/lib/i386-linux-gnu/libjpeg.so.8:
+
+/usr/lib/i386-linux-gnu/libjpeg.so.8:
+
+/lib/x86_64-linux-gnu/libjpeg.so.8:
+
+/lib64/ld-linux-x86-64.so.2:
+
+/lib/i386-linux-gnu/libxcb.so.1:
+
+/usr/lib/i386-linux-gnu/libxcb.so.1:
+
+/lib/x86_64-linux-gnu/libxcb.so.1:
+
+/lib/i386-linux-gnu/libXext.so.6:
+
+/usr/lib/i386-linux-gnu/libXext.so.6:
+
+/lib/x86_64-linux-gnu/libXext.so.6:
+
+/lib/i386-linux-gnu/libwebp.so.7:
+
+/usr/lib/i386-linux-gnu/libwebp.so.7:
+
+/lib/x86_64-linux-gnu/libwebp.so.7:
+
+/lib/i386-linux-gnu/libzstd.so.1:
+
+/usr/lib/i386-linux-gnu/libzstd.so.1:
+
+/lib/x86_64-linux-gnu/libzstd.so.1:
+
+/lib/i386-linux-gnu/liblzma.so.5:
+
+/usr/lib/i386-linux-gnu/liblzma.so.5:
+
+/lib/x86_64-linux-gnu/liblzma.so.5:
+
+/lib/x86_64-linux-gnu/libLerc.so.4:
+
+/lib/i386-linux-gnu/libjbig.so.0:
+
+/usr/lib/i386-linux-gnu/libjbig.so.0:
+
+/lib/x86_64-linux-gnu/libjbig.so.0:
+
+/lib/i386-linux-gnu/libdeflate.so.0:
+
+/usr/lib/i386-linux-gnu/libdeflate.so.0:
+
+/lib/x86_64-linux-gnu/libdeflate.so.0:
+
+/lib/i386-linux-gnu/libz.so.1:
+
+/usr/lib/i386-linux-gnu/libz.so.1:
+
+/lib/x86_64-linux-gnu/libz.so.1:
+
+/lib/i386-linux-gnu/libXau.so.6:
+
+/usr/lib/i386-linux-gnu/libXau.so.6:
+
+/lib/x86_64-linux-gnu/libXau.so.6:
+
+/lib/i386-linux-gnu/libXdmcp.so.6:
+
+/usr/lib/i386-linux-gnu/libXdmcp.so.6:
+
+/lib/x86_64-linux-gnu/libXdmcp.so.6:
+
+/lib/i386-linux-gnu/libsharpyuv.so.0:
+
+/usr/lib/i386-linux-gnu/libsharpyuv.so.0:
+
+/lib/x86_64-linux-gnu/libsharpyuv.so.0:
+
+/lib/i386-linux-gnu/libbsd.so.0:
+
+/usr/lib/i386-linux-gnu/libbsd.so.0:
+
+/lib/x86_64-linux-gnu/libbsd.so.0:
+
+/lib/i386-linux-gnu/libmd.so.0:
+
+/usr/lib/i386-linux-gnu/libmd.so.0:
+
+/lib/x86_64-linux-gnu/libmd.so.0:
